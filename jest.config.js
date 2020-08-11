2a3ace22634d11c9ae85918d8f64621ad0becdd8
André Martins <andre.felipe272@gmail.com>
2020-08-11 16:25:57 -0300

    chore: adicionado configurações para funcionar o import nos testes unitários

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'node',
-    transform: {
-        '.+\\.ts$': 'ts-jest'
-    }
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest'
+  }
 }
